5a0d87f6ce2a13c2fa903f5c5fce9941d17d2e54
Race Carpenter <racecarpenter@gmail.com>
2017-09-15 14:19:54 -0700

    C# practice

--- exampleApi/Controllers/UserController.cs
+++ exampleApi/Controllers/UserController.cs
@@ -14,17 +14,29 @@ namespace exampleApi.Controllers
 		{
             
 			_context = context;
+
+            if(_context.Users.Count() < 1)
+            {
+				User person = new User { Name = "Item1" };
+				_context.Users.Add(person);
+                _context.Users.Add(new User { Name = "yiggy" });
+				_context.SaveChanges(); 
+            }
 			
 		}
 		// GET: /<controller>/
         [HttpGet]
-        public ActionResult Add()
+        public ActionResult GetAll()
         {
-            User person = new User { Name = "Item1" };
-			_context.Users.Add(person);
-			_context.SaveChanges();
-			return Json(person);
+
+            return Json(_context.Users);
 
 		}
+        // get id
+        [HttpGet("{id}")]
+        public ActionResult GetOne(long id)
+        {
+            return Json(_context.Users.Where(user => user.Id == id).ToList()[0]);  
+        }
     }
 }
